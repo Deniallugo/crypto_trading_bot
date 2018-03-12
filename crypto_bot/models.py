976ab3885fd39045cc4d182d14dc050010b8e271
deniallugo <deniallugo@gmail.com>
2018-03-12 20:01:33 +1000

    add view to data

--- crypto_bot/models.py
+++ crypto_bot/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+from crypto_bot.decorators import cache_for
 
 
 class CoinPair(models.Model):
@@ -6,13 +9,6 @@ class CoinPair(models.Model):
     secondCoinName = models.CharField(max_length=255)
     exchange_code = models.CharField(max_length=255)
     active = models.BooleanField(default=True)
-
-
-class CoingySettings(models.Model):
-    api_key = models.CharField(max_length=255)
-    secret_key = models.CharField(max_length=255)
-    main = models.BooleanField(default=False)
-    min_wall = models.FloatField(null=True)
     position_type = models.CharField(choices=(
         ('history', 'История'),
         ('asks', 'asks'),
@@ -21,6 +17,21 @@ class CoingySettings(models.Model):
         ('all', 'all'),
 
     ), max_length=6)
+
+    def __str__(self):
+        return f'{self.firstCoinName}/{self.secondCoinName}'
+
+
+class CoingySettings(models.Model):
+    api_key = models.CharField(max_length=255)
+    secret_key = models.CharField(max_length=255)
+    main = models.BooleanField(default=False)
+    min_wall = models.FloatField(null=True)
+
+    @classmethod
+    @cache_for(60 * 5)
+    def main_setting(cls):
+        return cls.objects.filter(main=True)[0]
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
@@ -32,8 +43,11 @@ class CoingySettings(models.Model):
             except self.DoesNotExist:
                 pass
 
-        super(self, self.__class__).save(force_insert, force_update, using,
+        super(self.__class__, self).save(force_insert, force_update, using,
                                          update_fields)
+
+    def __str__(self):
+        return self.api_key
 
 
 class MarketPosition(models.Model):
@@ -44,3 +58,21 @@ class MarketPosition(models.Model):
     total = models.FloatField()
     position_type = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    @cache_for(60 * 60 * 24)
+    def avg_quantity_day(cls):
+        return cls.objects.filter(updated_at__gte=timezone.now() -
+                                                  datetime.timedelta(
+                                                      days=1)).aggregate(
+            avg_day=models.Avg('quantity'))['avg_day']
+
+    @property
+    @cache_for(60 * 60)
+    def wall(self):
+        return float(self.quantity) > min(self.avg_quantity_day() * 100,
+                                          CoingySettings.main_setting().min_wall)
+
+    def __str__(self):
+        return str(self.price)
--- crypto_bot/views.py
+++ crypto_bot/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.core import serializers
+from crypto_bot.models import MarketPosition
+from django.http import JsonResponse
+import datetime
+from django.core.serializers.json import DjangoJSONEncoder
+
+
+def get_data(request):
+    start_date = int(request.GET.get('start_date', 0))
+    wall = request.GET.get('wall', False)
+    start_date = datetime.datetime.utcfromtimestamp(start_date)
+    market_position = MarketPosition.objects.filter(
+        updated_at__gte=start_date, wall=wall)
+    data = serializers.serialize('json', market_position,
+                                 cls=DjangoJSONEncoder)
+    return JsonResponse(data, safe=False)
+
